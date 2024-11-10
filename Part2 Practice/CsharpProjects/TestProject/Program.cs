58953bd6ec7a8c201964d2910714c45a8faad369
Samandeep246 <smngill246@gmail.com>
2024-11-10 17:51:12 -0500

    Update Program.cs

--- Part2 Practice/CsharpProjects/TestProject/Program.cs
+++ Part2 Practice/CsharpProjects/TestProject/Program.cs
@@ -1,19 +1,80 @@
-﻿Random random = new Random();
-string[] orderIDs = new string[5];
-// Loop through each blank orderID
-for (int i = 0; i < orderIDs.Length; i++)
+﻿using System;
+
+namespace DiceRoll
 {
-    // Get a random value that equates to ASCII letters A through E
-    int prefixValue = random.Next(65, 70);
-    // Convert the random value into a char, then a string
-    string prefix = Convert.ToChar(prefixValue).ToString();
-    // Create a random number, pad with zeroes
-    string suffix = random.Next(1, 1000).ToString("000");
-    // Combine the prefix and suffix together, then assign to current OrderID
-    orderIDs[i] = prefix + suffix;
-}
-// Print out each orderID
-foreach (var orderID in orderIDs)
-{
-    Console.WriteLine(orderID);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // DiceRoll();
+           // findLarger();
+           //decisonLogic();
+           arrayandForeach();
+        }
+        static void DiceRoll()
+        {
+            Random dice = new Random();
+            int roll1 = dice.Next();
+            int roll2 = dice.Next(101);
+            int roll3 = dice.Next(50, 101);
+
+            Console.WriteLine($"First roll: {roll1}");
+            Console.WriteLine($"Second roll: {roll2}");
+            Console.WriteLine($"Third roll: {roll3}");
+
+        }
+        static void findLarger()
+        {
+            int firstValue = 500;
+            int secondValue = 600;
+            int largerValue;
+            largerValue = Math.Max(firstValue, secondValue);
+            Console.WriteLine($"larger no is ::{largerValue}");
+        }
+        static void decisonLogic()
+        {
+            Random random = new Random();
+            int daysUntilExpiration = random.Next(12);
+            int discountPercentage = 0;
+
+            if (daysUntilExpiration == 0)
+            {
+                Console.WriteLine("Your subscription has expired.");
+            }
+            else if (daysUntilExpiration == 1)
+            {
+                Console.WriteLine("Your subscription expires within a day!");
+                discountPercentage = 20;
+            }
+            else if (daysUntilExpiration <= 5)
+            {
+                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+                discountPercentage = 10;
+            }
+            else if (daysUntilExpiration <= 10)
+            {
+                Console.WriteLine("Your subscription will expire soon. Renew now!");
+            }
+
+            if (discountPercentage > 0)
+            {
+                Console.WriteLine($"Renew now and save {discountPercentage}%.");
+            }
+
+
+        }
+        static void arrayandForeach()
+        {
+            string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+            foreach (string orderID in orderIDs)
+            {
+                if (orderID.StartsWith("C"))
+                {
+                    Console.WriteLine(orderID);
+                }
+            }
+        }
+    }
+
 }
